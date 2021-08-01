ce2445cd74230cbc93b90130de552b9c02eefdf5
amulyasagi <87672966+amulyasagi@users.noreply.github.com>
2021-08-01 10:53:52 -0400

    Updated VPC Module

--- dev-ec2.tf
+++ dev-ec2.tf
@@ -1,6 +1,6 @@
 module "dev-ec2" {
 
- source = "git@github.com:amulyasagi/tfmodule-ec2.git"
+ source = "git@github.com:amulyasagi/tfmodule-ec2.git?ref=1.0"
  
  ami = var.ami
  ec2_class = var.ec2_class
@@ -9,6 +9,8 @@ module "dev-ec2" {
  product = var.product
  environment = var.environment
  vpc_id = var.vpc_id
+ zone = var.dev_zone
+ ebssize = var.dev_ebssize
  
  }
 
@@ -19,10 +21,13 @@ module "dev-ec2" {
  ami = var.ami
  ec2_class = var.ec2_class2
  key_pair = var.key_pair
- subnet_1a = var.subnet_1a
+ subnet_1a = var.subnet_1c
  product = var.product2
  environment = var.environment2
  vpc_id = var.vpc_id
+ zone = var.qa_zone
+ ebssize = var.qa_ebssize
+ 
  
  }
 
